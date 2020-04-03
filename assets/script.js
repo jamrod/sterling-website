b576be2f8819b4ff1277388716cc2e0c059fd5ca
Jamrod <myfavoritenoisemaker@gmail.com>
2020-04-03 16:30:51 -0600

    mobile nav, other mobile style adjusts

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,15 @@
 console.log("running")
+
+const mobileNav = document.querySelector('#mobile-nav')
+
+let hidden = true
+
+const cycleNav = () => {
+    if (hidden) {
+        mobileNav.style.display = 'block'
+    }
+    else {
+        mobileNav.style.display = 'none'
+    }
+    hidden = !hidden
+}
